1ac039ddf0a764312d533188b27a63cc5018fd83
mustafa <mustafadervisoglu16@gmail.com>
2023-01-02 10:17:42 +0300

    sendAsync have been added

--- src/SignMessage.js
+++ src/SignMessage.js
@@ -12,7 +12,7 @@ const signMessage = async ({ setError, message }) => {
     if (!window.ethereum)
       throw new Error("No crypto wallet found. Please install it.");
 
-    await window.ethereum.send("eth_requestAccounts");
+    await window.ethereum.sendAsync("eth_requestAccounts");
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const signature = await signer.signMessage(message);
